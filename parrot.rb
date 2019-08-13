68951436eb60444df0f74b232c6917643fcb0545
clean-command-1851 <ed7901979t@gmail.com>
2019-08-13 01:37:04 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+def parrot(my_string="Squawk!")
+  puts "The my_string is #{my_string}"
+  return my_string
+end
+parrot(my_string="Hello")
